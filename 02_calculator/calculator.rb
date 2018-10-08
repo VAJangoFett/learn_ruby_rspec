6c6cbcb9a5c45e66607ddf73c41f3ada22c27880
Jango <arthur.vouloir@gmail.com>
2018-10-08 14:55:55 +0200

    fin exo 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,42 @@
 #write your code here
+def add (num1,num2)
+	num1 + num2
+end
+
+def subtract (num1,num2)
+	num1 - num2
+end
+
+def sum (number)
+	count = 0
+		number.each do |num|
+			count = count + num
+		end
+	return count.to_i
+end
+
+def multiply (num1,num2)
+	num1 * num2
+end
+
+def power (num1,num2)
+	num1 ** num2
+end
+
+# def factorial (num)
+# 	num.times do |n|
+# 		# if n == 0
+# 		# 	n += 1
+# 		# elsif n == 1
+# 		# 	n = 1
+# 		# else
+# 		n * (n - 1)
+# 		end
+
+# end
+
+
+
+
+
+
