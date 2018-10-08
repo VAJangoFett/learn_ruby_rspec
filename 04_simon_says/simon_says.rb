5f2ab50692a4f754762390d54b7f70ec779d3e1e
Jango <arthur.vouloir@gmail.com>
2018-10-08 18:49:51 +0200

    fin exo 4

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,39 @@
-#write your code here
+
+def echo(i)
+	"#{i}"
+end 
+
+def shout(i)
+	i.upcase
+end 
+
+def repeat(string, num=2)
+	a = (string + " ") * num
+	a.chop
+end 
+
+def start_of_word(word, num)
+	word.slice(0...num)
+end 
+
+def first_word(word)
+	(word.split(" ")).slice(0)
+end 
+
+def titleize(word)
+	word.capitalize!
+	words_no_cap = ["and", "the"]
+	word.split(" ").map{ |word|
+		if words_no_cap.include?(word)
+			word
+		else
+			word.capitalize
+		end
+	}.join(" ")
+end 
+
+
+
+
+
+
